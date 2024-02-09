fc0f75b1983b5a93efde4c85a929a98b4ce51591
fitiarivony <92774199+fitiarivony@users.noreply.github.com>
2024-02-09 10:40:43 +0300

    crud-service skeleton

---
+++ src/app/service/update-service/update-service.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Service } from 'src/app/interfaces/service';
+import { ServeService } from '../../services/serve.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-update-service',
+  templateUrl: './update-service.component.html',
+  styleUrls: ['./update-service.component.css']
+})
+export class UpdateServiceComponent {
+  service:Service={
+    _id:"",
+    nom_service:"",
+    prix:0,
+    duree:0,
+    comission:0,
+  }
+  onlyNumber(){
+
+  }
+  updateService(){
+      this.serveService.updateService(this.service).subscribe({
+        next:valiny=>{
+          this.router.navigate(['/services/list']);
+        },
+        error:err=>console.log(err.error)
+      })
+  }
+  ngOnInit(){
+    this.route.params.subscribe(params => {
+      let id_service = params['id']; // Access the 'id' parameter from the URL
+      this.serveService.getServiceById(id_service).subscribe({
+        next:valiny=>this.service=valiny,
+        error:err=>console.log(err.message)
+      })
+    });
+    // this.serveService.getServiceById()
+  }
+  constructor(private serveService:ServeService,private route: ActivatedRoute,private router:Router) {
+
+  }
+}
---
+++ src/app/services/serve.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Service } from '../interfaces/service';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServeService {
+  constructor(private http: HttpClient) {}
+  services: Service[] | [] = [];
+  listService() {
+    let val = this.http.get<Service[]>('http://localhost:8000/services');
+    return val;
+  }
+  createService(service: Service): Observable<Service> {
+    return this.http.post<Service>('http://localhost:8000/services', service, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    });
+  }
+
+  getServiceById(id: string) {
+    return this.http.get<Service>('http://localhost:8000/services/' + id, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    });
+  }
+  updateService(service: Service) {
+    return this.http.put<Service>('http://localhost:8000/services/'+service._id, service, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    });
+  }
+  deleteService(_id: string){
+    return this.http.delete('http://localhost:8000/services/'+_id,{headers:{
+      Authorization: 'Bearer '+localStorage.getItem('token'),
+    }});
+  }
+}
