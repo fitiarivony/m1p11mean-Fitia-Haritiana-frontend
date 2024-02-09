73328257f16c5c20ba663aef1f90e5c9e0901b82
fitiarivony <92774199+fitiarivony@users.noreply.github.com>
2024-02-09 10:47:50 +0300

    update imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { LoginEmpComponent } from './emp/login-emp/login-emp.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginManagerComponent } from './manager/login-manager/login-manager.component';
+import { ListServiceComponent } from './service/list-service/list-service.component';
+import { CreateServiceComponent } from './service/create-service/create-service.component';
+import { UpdateServiceComponent } from './service/update-service/update-service.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginEmpComponent }
   { path: 'login-manager', component: LoginManagerComponent },
-
+  {path:'services/list',component: ListServiceComponent},
+  {path:'services/create',component: CreateServiceComponent},
+  {path:'services/update/:id',component: UpdateServiceComponent},
 ];
 
 @NgModule({
