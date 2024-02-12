c6b4878fa9073d4de50d4a4b9b4ae5de059cf5fa
fitiarivony <92774199+fitiarivony@users.noreply.github.com>
2024-02-12 14:07:16 +0300

    login+inscription client

---
+++ src/app/clients/login-client/login-client.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Client } from 'src/app/interfaces/client';
+import { ClientService } from 'src/app/services/client.service';
+
+@Component({
+  selector: 'app-login-client',
+  templateUrl: './login-client.component.html',
+  styleUrls: ['./login-client.component.css']
+})
+export class LoginClientComponent {
+  client: Client = {
+    _id: '',
+    nom_client: '',
+    prenom_client: '',
+    identifiant: '',
+    mdp: '',
+    numero: '',
+  };
+  constructor(private client_service: ClientService, private router: Router) {}
+  loginClient() {
+    this.client_service.loginClient(this.client).subscribe({
+      next: (client:any) => {
+        console.log(client);
+
+        localStorage.setItem('token',client.token.token)
+        this.router.navigate(['/prise-rdv']);
+      },
+      error: (err) => console.log(err.error),
+    });
+ 
+  }
+}
---
+++ src/app/interfaces/client.ts
@@ -0,0 +1,8 @@
+export interface Client {
+  _id: string;
+  nom_client: string;
+  prenom_client: string;
+  identifiant: string;
+  mdp: string;
+  numero: string;
+}
