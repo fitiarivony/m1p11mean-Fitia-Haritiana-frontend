748418b1be685de66b184fca20271391657dad84
fitiarivony <92774199+fitiarivony@users.noreply.github.com>
2024-02-21 10:04:03 +0300

    update is_done rdv

--- src/app/clients/historique-rdv/historique-rdv.component.ts
+++ src/app/clients/historique-rdv/historique-rdv.component.ts
@@ -8,53 +8,62 @@ import { Rdv_Service } from 'src/app/services/rdv.service';
 @Component({
   selector: 'app-historique-rdv',
   templateUrl: './historique-rdv.component.html',
-  styleUrls: ['./historique-rdv.component.css']
+  styleUrls: ['./historique-rdv.component.css'],
 })
 export class HistoriqueRdvComponent {
-  historique:FullRdv[]=[]
-  _id:string=""
-  constructor(private clientService:ClientService,private rdvservice:Rdv_Service,
-    private route: ActivatedRoute) {
-
-  }
+  historique: FullRdv[] = [];
+  _id: string = '';
+  constructor(
+    private clientService: ClientService,
+    private rdvservice: Rdv_Service,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this._id = localStorage.getItem('id')!
+    this._id = localStorage.getItem('id')!;
     this.clientService.getHistorique(this._id).subscribe({
-      next: v => {
-        v.map((el: { date_rdv: string | number | Date; })=>{
-          el.date_rdv=new Date(el.date_rdv)
-        })
-        this.historique = v
+      next: (v) => {
+        v.map((el: { date_rdv: string | number | Date }) => {
+          el.date_rdv = new Date(el.date_rdv);
+        });
+        this.historique = v;
       },
-      error: err => {
-        console.log(err)
-      }
-    })
-  }
-  annulerRdv(idRdv: string){
-    this.rdvservice.annulerRdv(idRdv).subscribe({
-      next: v => {
-        let historiques=[...this.historique]
-        this.historique=historiques.filter(h=>h._id!=idRdv);
-      },error: err => {
-        console.log(err.error);
-
-      }
-
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
-  returnCancellable(date_rdv:string){
-    return new Date(date_rdv)>new Date();
+  annulerRdv(idRdv: string) {
+    this.rdvservice.annulerRdv(idRdv).subscribe({
+      next: (v) => {
+        let historiques = [...this.historique];
+        this.historique = historiques.filter((h) => h._id != idRdv);
+      },
+      error: (err) => {
+        console.log(err.error);
+      },
+    });
   }
-  payerRdv(idRdv:string){
-    console.log("Je paie");
-
-      this.rdvservice.payerRdv(idRdv).subscribe({
-        next:val=>console.log(val),
-        error:err=>console.log(err.error)
-      })
+  returnCancellable(date_rdv: string) {
+    return new Date(date_rdv) > new Date();
   }
+  payerRdv(idRdv: string) {
+    console.log('Je paie');
+    let filteredArray = this.historique.filter(obj => obj._id === idRdv);
+    // Get the index of an object in the array
+    let index = this.historique.indexOf(filteredArray[0]);
 
+    this.rdvservice.payerRdv(idRdv).subscribe({
+      next: (val) => {
+        let historiques=[...this.historique]
+        historiques[index].paye=true;
+        this.historique=historiques;
+        console.log(val)
+      },
+      error: (err) => console.log(err.error),
+    });
+
+
+  }
 }
--- src/app/emp/affichage-rdv/affichage-rdv.component.ts
+++ src/app/emp/affichage-rdv/affichage-rdv.component.ts
@@ -27,6 +27,19 @@ export class AffichageRdvComponent implements OnInit {
     if (new Date(datefin)>new Date() && new Date(datedebut)<new Date()) return 0
     return -1;
   }
+  marquer_vita(item:RdvFull){
+    this.rdv_service.suivi_tache(item.id_rdv,item.id_employe._id,item.id_service._id,!item.is_done).subscribe({
+      next:val=>{
+        let filteredArray = this.rdv.filter(obj => obj === item);
+        // Get the index of an object in the array
+        let index = this.rdv.indexOf(filteredArray[0]);
+        let rdvs=[...this.rdv]
+        rdvs[index].is_done=!item.is_done;
+        this.rdv=rdvs;
+      },
+      error:err =>{console.log("error")},
+    })
+  }
 
 
 }
